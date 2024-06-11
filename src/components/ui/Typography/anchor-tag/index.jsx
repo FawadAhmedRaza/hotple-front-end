96c9624cb9dbd1bd0687bc5241cd6a961ddff08d
jawad-ahmed1997 <jawad.walbi@gmail.com>
2024-06-11 17:07:44 +0500

    pull from arsalan

--- src/components/ui/Typography/anchor-tag/index.jsx
+++ src/components/ui/Typography/anchor-tag/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AnchotTag = ({ children, className, link }) => {
     return (
-        <a href={link} className={` text-15fs md:text-base leading-6  dark:text-dark_link_label text-light_link_label ${className}`}>{children}</a>
+        <a href={link} className={` hover:underline cursor-pointer text-15fs md:text-base leading-6  dark:text-dark_link_label text-light_link_label ${className}`}>{children}</a>
     )
 }
 
--- src/sections/auth/userLoginView.js
+++ src/sections/auth/userLoginView.js
@@ -1,16 +1,18 @@
 "use client"
 import React from 'react'
-import RHFTextInput from "@/components/inputs/RHFTextfiels";
+import RHFTextInput from "@/components/rhf-hooks/RHFTextfiels";
 import Modal from "@/components/modal";
 
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import SolidButton from "@/components/ui/Buttons/solid-button";
-import RHFFormProvider from "@/components/inputs/Form";
+import RHFFormProvider from "@/components/rhf-hooks/Form";
 import { LoginUser } from "@/api/auth";
 import { jwtDecode } from 'jwt-decode';
 import { useAuthContext } from '@/context/auth/useAuthContext';
+import Paragraph from '@/components/ui/Typography/paragraph';
+import AnchotTag from '@/components/ui/Typography/anchor-tag';
 
 const UserLoginView = ({isOpen,setIsOpen}) => {
   const closeModal = (position) => {
@@ -73,6 +75,15 @@ const UserLoginView = ({isOpen,setIsOpen}) => {
           <SolidButton type="submit"  className='w-full bg-red font-bold my-2' >
             Log In
           </SolidButton>
+        </div>
+        <div className='flex flex-row justify-between items-center'>
+        
+          <Paragraph className="dark:!text-dark_primary_label">
+          Don't have an account? <AnchotTag>Sign Up</AnchotTag>
+          </Paragraph>
+          <Paragraph className="dark:!text-dark_primary_label">
+           <AnchotTag>Forgot password</AnchotTag>
+          </Paragraph>
         </div>
     </RHFFormProvider>
       </div>
