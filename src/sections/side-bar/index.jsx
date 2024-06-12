ac9a0d0032f0242213339e69ab315afccb25b72b
jawad-ahmed1997 <jawad.walbi@gmail.com>
2024-06-13 03:55:28 +0500

    pull rabeet

--- src/sections/side-bar/index.jsx
+++ src/sections/side-bar/index.jsx
@@ -16,25 +16,25 @@ const SideBar = () => {
   const tabs = [
     {
       id: '1',
-      title: '发现',
+      title: '발견하다',
       icon: 'solar:home-smile-linear',
       link: '/authenticated/user/post/new',
     },
     {
       id: '2',
-      title: '发布',
+      title: '풀어 주다',
       icon: 'mynaui:plus-square',
       link: '/user/post/new',
     },
     {
       id: '3',
-      title: '通知',
+      title: '통지하다',
       icon: 'ci:bell',
       link: 'user/post/new',
     },
     {
       id: '4',
-      title: '우편',
+      title: '대답',
       icon: 'ci:bell',
       link: '/user/post/new',
     },
@@ -43,22 +43,22 @@ const SideBar = () => {
   const loginData = [
     {
       id: '1',
-      title: '刷到更懂你的优质内容',
+      title: '귀하를 더 잘 이해할 수 있는 고품질 콘텐츠를 찾아보세요.',
       icon: 'iconamoon:like-light',
     },
     {
       id: '2',
-      title: '搜索最新种草、拔草信息',
+      title: '식재 및 제초에 관한 최신 정보를 검색해 보세요.',
       icon: 'ph:plant-light',
     },
     {
       id: '3',
-      title: '查看收藏、点赞的笔记',
+      title: '즐겨찾기 및 좋아요 표시한 메모 보기',
       icon: 'hugeicons:star',
     },
     {
       id: '4',
-      title: '与他人更好地互动、交流',
+      title: '다른 사람들과 더 잘 소통하고 소통하세요.',
       icon: 'basil:comment-outline',
     },
   ]
@@ -74,15 +74,14 @@ const SideBar = () => {
         <div className="flex flex-col gap-2 ">
           {tabs?.map((tab, index) => (
             <Link href={tab?.link}>
-
             <IconButton key={index} icon={tab?.icon} iconClass={' !w-22wd '} className={` ${tab.title == activeTab ? 'dark:bg-dark_bg_grey bg-light_bg_grey' : ''}`} onClick={() => setActiveTab(tab.title)}>{tab?.title}</IconButton>
             </Link>
           ))}
         </div>
         <div className="flex flex-col gap-3 ">
-          <SolidButton className={'w-full'}>登录</SolidButton>
+          <SolidButton className={'w-full'}>로그인</SolidButton>
           <div className="flex flex-col gap-1 border dark:text-dark_primary_label dark:border-neutral-800 border-neutral-200 rounded-2xl px-4 py-2.5 ">
-            <H6 className={'mb-1 dark:!text-dark_secondary_label !text-light_secondary_label'}>马上登录即可</H6>
+            <H6 className={'mb-1 dark:!text-dark_secondary_label !text-light_secondary_label'}>지금 로그인하십시오</H6>
             {loginData?.map((item, index) => (
      
                     <div key={index} className='flex gap-2 items-center'>
@@ -98,7 +97,7 @@ const SideBar = () => {
       </div>
       <div className="mb-0">
         <DropdownMenu> 
-          <IconButton icon={'fluent:navigation-24-filled'} className={'w-full'}>更多 </IconButton>
+          <IconButton icon={'fluent:navigation-24-filled'} className={'w-full'}>더 </IconButton>
         </DropdownMenu>
       </div>
     </Div>
