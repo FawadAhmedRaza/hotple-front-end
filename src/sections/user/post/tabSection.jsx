b708e5279505abd02a2be0b7d8c337a31452ca99
jawad-ahmed1997 <jawad.walbi@gmail.com>
2024-06-13 03:21:39 +0500

    image and vedio uploader

---
+++ src/sections/user/post/tabSection.jsx
@@ -0,0 +1,126 @@
+import { RHFVedioUpload } from "@/components/rhf-hooks/rhf-vedioUploader";
+import Tabs from "./tabs";
+import { RHFUpload } from "@/components/rhf-hooks/RHFUpload";
+import { useCallback } from "react";
+import H3 from "@/components/ui/Typography/h3";
+import H4 from "@/components/ui/Typography/h4";
+import Paragraph from "@/components/ui/Typography/paragraph";
+import H6 from "@/components/ui/Typography/h6";
+
+
+export default function TabSection({ setValue, watch }) {
+
+    const VedioUploadeer = ({ setValue, watch }) => {
+        const values = watch();
+        const handleDrop = useCallback(
+            (acceptedFiles) => {
+                const files = values.files || [];
+
+                const newFiles = acceptedFiles.map((file) =>
+                    Object.assign(file, {
+                        preview: URL.createObjectURL(file),
+                    })
+                );
+                setValue('files', [...files, ...newFiles], { shouldValidate: true });
+            },
+            [setValue, values.files]
+        );
+
+        const opreation = [
+                {title :" 비디오 크기",descreption:"지원 시간은 60분입니다. 최대 20GB 비디오 크기 "},
+                {title :" 비디오 형식",descreption:"지원 시간은 60분입니다. 최대 20GB 비디오 크기"},
+                {title :" 비디오 해상도",descreption:"지원 시간은 60분입니다. 최대 20GB 비디오 크기"}
+        ]
+        return (
+            <div className="flex items-center justify-center flex-col   ">
+                <RHFVedioUpload
+                className='min-h-72'
+                    thumbnail
+                    name='files'
+                    maxSize={3145728}
+                    onDrop={handleDrop}
+                    onUpload={() => console.info('ON UPLOAD')} />
+                      <div className=" py-4 grid grid-cols-3">
+                        {opreation?.map((item,ind)=>(
+                            <div className="flex justify-center items-center flex-col  border-2  dark:border-brown border-light_bg_grey py-4 px-2 mx-1 rounded-lg ">
+                                <H6 className="text-center !font-semibold">
+                                     {item?.title}
+                                </H6>
+                                <Paragraph className="text-center !text-xs">
+                                     {item?.descreption}
+                                </Paragraph>
+                            </div>
+                        ))}
+                    </div>
+            </div>
+        )
+    }
+    const ImageUploadeer = ({ setValue, watch }) => {
+        const values = watch();
+        const handleDrop = useCallback(
+            (acceptedFiles) => {
+                const files = values.files || [];
+
+                const newFiles = acceptedFiles.map((file) =>
+                    Object.assign(file, {
+                        preview: URL.createObjectURL(file),
+                    })
+                );
+
+                setValue('files', [...files, ...newFiles], { shouldValidate: true });
+            },
+            [setValue, values.files]
+        );
+        const opreation = [
+            {title :" 이미지 크기",descreption:"지원 시간은 60분입니다. 최대 20GB 비디오 크기 "},
+            {title :" 이미지 형식",descreption:"지원 시간은 60분입니다. 최대 20GB 비디오 크기 "},
+            {title :" 이미지 해상도",descreption:"지원 시간은 60분입니다. 최대 20GB 비디오 크기 "}
+    ]
+
+        return (
+            <div className="flex items-center justify-center flex-col  ">
+                <RHFUpload
+                    className='min-h-72 '
+                    thumbnail
+                    multiple
+                    name="files"
+                    maxSize={3145728}
+                    onDrop={handleDrop}
+                    onUpload={() => console.info('ON UPLOAD')} />
+                     <div className=" py-4 grid grid-cols-3">
+                        {opreation?.map((item,ind)=>(
+                            <div className="flex justify-center items-center flex-col border-2  dark:border-brown border-light_bg_grey  py-4 px-2 mx-1 rounded-lg ">
+                                <H6 className="text-center !font-semibold">
+                                     {item?.title}
+                                </H6>
+                                <Paragraph className="text-center !text-xs">
+                                     {item?.descreption}
+                                </Paragraph>
+                            </div>
+                        ))}
+                    </div>
+            </div>
+        )
+    }
+
+    const tabs = [
+        {
+            label: '동영상 업로드',
+            content: <VedioUploadeer setValue={setValue} watch={watch} />
+            ,
+        },
+        {
+            label: '사진 게시',
+            content: <ImageUploadeer setValue={setValue} watch={watch} />,
+        },
+
+    ];
+
+    return (
+        <div className=" flex items-center justify-center ">
+            <main className="w-full min-h-96">
+                <Tabs tabs={tabs} />
+            </main>
+        </div>
+    );
+}
---
+++ src/sections/user/post/tabs.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+const Tabs = ({ tabs }) => {
+  const [activeTab, setActiveTab] = useState(tabs[0].label);
+
+  const handleTabClick = (label) => {
+    setActiveTab(label);
+  };
+
+  return (
+    <div className="w-full max-w-lg mx-auto ">
+      <div className="flex ">
+        {tabs.map((tab) => (
+          <button
+            key={tab.label}
+            onClick={() => handleTabClick(tab.label)}
+            className={`py-2 px-4 focus:outline-none font-semibold ${
+              activeTab === tab.label
+                ? 'border-b-2 border-red text-red'
+                : 'text-gray-500'
+            }`}
+          >
+            {tab.label}
+          </button>
+        ))}
+      </div>
+      <div className="mt-4">
+        {tabs.map((tab) => (
+          <div
+            key={tab.label}
+            className={`${
+              activeTab === tab.label ? 'block' : 'hidden'
+            }`}
+          >
+            {tab.content}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Tabs;
