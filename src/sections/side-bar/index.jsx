79b4a59bfc56a66157136a3f2a6b9eb728bed937
jawad-ahmed1997 <jawad.walbi@gmail.com>
2024-06-12 18:42:40 +0500

    pull code

--- src/sections/side-bar/index.jsx
+++ src/sections/side-bar/index.jsx
@@ -6,6 +6,7 @@ import Iconify from '@/components/ui/Iconify-icons/Iconify'
 import H6 from '@/components/ui/Typography/h6'
 import Div from '@/components/ui/div'
 import DropdownMenu from '@/components/dropdown-menu'
+import Link from 'next/link'
 
 
 const SideBar = () => {
@@ -17,19 +18,25 @@ const SideBar = () => {
       id: '1',
       title: '发现',
       icon: 'solar:home-smile-linear',
-      link: 'Notify',
+      link: '/user/post/new',
     },
     {
       id: '2',
       title: '发布',
       icon: 'mynaui:plus-square',
-      link: 'Notify',
+      link: '/user/post/new',
     },
     {
       id: '3',
       title: '通知',
       icon: 'ci:bell',
-      link: '',
+      link: 'user/post/new',
+    },
+    {
+      id: '4',
+      title: '우편',
+      icon: 'ci:bell',
+      link: '/user/post/new',
     },
   ]
 
@@ -66,7 +73,10 @@ const SideBar = () => {
         {/* Tabs div  */}
         <div className="flex flex-col gap-2 ">
           {tabs?.map((tab, index) => (
+            <Link href={tab?.link}>
+
             <IconButton key={index} icon={tab?.icon} iconClass={' !w-22wd '} className={` ${tab.title == activeTab ? 'dark:bg-dark_bg_grey bg-light_bg_grey' : ''}`} onClick={() => setActiveTab(tab.title)}>{tab?.title}</IconButton>
+            </Link>
           ))}
         </div>
         <div className="flex flex-col gap-3 ">
@@ -74,10 +84,14 @@ const SideBar = () => {
           <div className="flex flex-col gap-1 border dark:text-dark_primary_label dark:border-neutral-800 border-neutral-200 rounded-2xl px-4 py-2.5 ">
             <H6 className={'mb-1 dark:!text-dark_secondary_label !text-light_secondary_label'}>马上登录即可</H6>
             {loginData?.map((item, index) => (
-              <div key={index} className='flex gap-2 items-center'>
-                <Iconify icon={item.icon} className={'!w-4 !h-4 dark:text-dark_tertiary_label text-neutral-500  '} />
-                <H6 className={'dark:!text-dark_tertiary_label !text-neutral-500 '}>{item.title}</H6>
-              </div>
+     
+                    <div key={index} className='flex gap-2 items-center'>
+              
+              <Iconify icon={item.icon} className={'!w-4 !h-4 dark:text-dark_tertiary_label text-neutral-500  '} />
+              <H6 className={'dark:!text-dark_tertiary_label !text-neutral-500 '}>{item.title}</H6>
+            </div>
+              
+        
             ))}
           </div>
         </div>
